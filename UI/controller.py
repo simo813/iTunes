f46677d848fcee8145530ee297274092eab1f7b8
Giuseppe Averta <g.averta3@gmail.com>
2025-05-23 12:05:50 +0200

    first commit

---
+++ UI/controller.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+    def handleCreaGrafo(self, e):
+        pass
+
+    def getSelectedAlbum(self, e):
+        pass
+
+    def handleAnalisiComp(self, e):
+        pass
+
+    def handleGetSetAlbum(self, e):
+        pass
